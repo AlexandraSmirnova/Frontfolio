c3e0f64c7fdfbff83f500370841a3a5f55f1c333
Смирнова Александра <a.smirnova@corp.mail.ru>
2026-03-02 18:27:36 +0300

    add content

--- src/components/Slider/Slide/index.tsx
+++ src/components/Slider/Slide/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import styles from './styles.css';
 
@@ -7,11 +8,15 @@ export interface SlideProps {
     name: string;
     description: string;
     image: string;
+    isContainBackground?: boolean;
 }
 
-export const Slide: React.FC<SlideProps> = ({ name, description, image }) => {
+export const Slide: React.FC<SlideProps> = ({ name, description, image, isContainBackground = false }) => {
     return (
-        <div className={styles.slide} style={{ backgroundImage: `url(/images/${image})`}}>
+        <div
+            className={classNames(styles.slide, {[styles.isContain]: isContainBackground})}
+            style={{ backgroundImage: `url(/images/${image})`}}
+        >
             <div className={styles.footer}>
                 <h3>{name}</h3>
                 <div className={styles.description}>{description}</div>
